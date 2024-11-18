309a4a25ae3cb233ea8ae579a5a9ccbce4de8b80
Mariam0422 <mariam79.yan@gmail.com>
2024-11-18 17:40:14 +0400

    add basic sructure

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import './App.css'
+
+function App() {
+  return (
+    <div className="wrapper">
+     <Header/>
+     <Footer/>
+    </div>
+  )
+}
+
+export default App
